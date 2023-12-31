c50418dc39264968764b4421b8a055d5d9eb333c
Abdullaev <abdullaev@mjr.ru>
2023-12-31 21:48:35 +0300

    added gifs and questions

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var questions = [
-    { question: "Вопрос номер 1", correctAnswer: "CorrectAnswer1" },
-    { question: "Вопрос номер 2", correctAnswer: "CorrectAnswer2" },
-    { question: "Вопрос номер 3", correctAnswer: "CorrectAnswer3" }
+    { question: "Начнем с базы: чей год мы встречаем? (Какое животное?)", correctAnswer: "дракон" },
+    { question: "Тэкс, ну еще тогда, год-то високосный будет? (Да/нет)", correctAnswer: "да" },
+    { question: "Теперь загадки. Кого плотненько попустили в канун Нового года, и про кого теперь песня: 'Никаких больше вечеринок'", correctAnswer: "ивлеева" },
+    { question: "Страна в которой пьют ракию и постоянно делают полако. Мы целый год ее мусолим, но никак не можем поехать. (А еще билеты в нее стоят как тюнингованная приора)", correctAnswer: "сербия" },
+    { question: "Какую кинофраншизу мы смотрели на прошлых новогодних праздниках? И которая была очень даже неплоха (Ну по крайней мере первые 3 фильма, мяу)", correctAnswer: "гарри поттер" },
+    { question: "Хайчите?", correctAnswer: "не байчите" },
+    { question: "Мяу?", correctAnswer: "мяу" },
+    { question: "А угадаешь, чья музычка играет? (Пишишьте имя на кириллице)", correctAnswer: "тайлер" },
+    { question: "Ну и главный вопрос, а ты меня любишь? 👉👈", correctAnswer: "да" },
+    { question: "Очень? 👉👈", correctAnswer: "да" },
 ];
 
 var currentQuestionIndex = 0;
@@ -17,7 +24,7 @@ audioPlayer.style.bottom = '10px';
 audioPlayer.style.left = '10px';
 audioPlayer.innerHTML = `
     <source src="msc/Hot Chocolate.mp3" type="audio/mp3">
-`;
+`;  
 document.body.appendChild(audioPlayer);
 
 function displayQuestion() {
@@ -28,7 +35,7 @@ function displayQuestion() {
 }
 
 function validateAnswer() {
-    var userInput = document.getElementById('userInput').value;
+    var userInput = document.getElementById('userInput').value.toLowerCase();
     var correctAnswer = questions[currentQuestionIndex].correctAnswer;
     var errorBlock = document.getElementById('errorBlock');
 
@@ -52,15 +59,25 @@ function validateAnswer() {
                 <source src="msc/ChapaChapa.mp4" type="audio/mp4">
             `;
             
-            alert('All questions answered!');
+            alert('Уря! Ты правльно ответила на все вопросы, быстрее жмякай ОК и забирай подарок!!!!');
             
             var mediaContainer = document.createElement('div');
             mediaContainer.id = 'mediaContainer';
+
+            var gifClasses = ['giftop-left', 'giftop-right', 'gifbottom-left', 'gifbottom-right'];
+
+            for (var i = 1; i <= 4; i++) {
+                var gifElement = document.createElement('img');
+                gifElement.src = 'img/Chipi.gif';
+                gifElement.alt = 'Gif ' + i;
+                gifElement.className = 'surrounding-gif ' + gifClasses[i - 1];
+                mediaContainer.appendChild(gifElement);
+            }
+            
             
             var imageElement = document.createElement('img');
             imageElement.src = 'img/Cert.jpg';
             imageElement.alt = 'Congratulations Image';
-
             imageElement.width = 700;
             imageElement.height = 450;
             
@@ -78,6 +95,7 @@ function validateAnswer() {
             document.body.style.background = 'url("img/Bg1.gif")';
             document.body.style.backgroundSize = 'cover';
             document.body.style.backgroundRepeat = 'no-repeat';
+
         }
     } else {
         errorBlock.style.display = 'block';
@@ -89,10 +107,10 @@ function toggleMusic() {
 
     if (audioPlayer.paused) {
         audioPlayer.play();
-        toggleMusicBtn.innerText = 'Найс 😎';
+        toggleMusicBtn.innerText = 'Найс 😎, чтобы выключить ткни есчо раз';
     } else {
         audioPlayer.pause();
-        toggleMusicBtn.innerText = 'Грустновато без музычки так-то 😥';
+        toggleMusicBtn.innerText = 'Грустновато без музычки так-то 😥, чтобы включить жми сюда!';
     }
 }
 
